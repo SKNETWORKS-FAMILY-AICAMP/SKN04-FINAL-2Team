1ef04cf7b88f7cc81a4cc66b412a657899b6bc9d
Kwonohs <dhtpa95@gmail.com>
2024-12-20 14:27:37 +0900

    전체페이지 크롤링, 파일로 저장

--- backend/profile_manager/crawling.py
+++ backend/profile_manager/crawling.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
@@ -8,38 +6,43 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+
 from tqdm import tqdm
 
-url = "https://www.rallit.com/"
-response = requests.get(url, headers={'User-Agent': 'Mozilla 5.0'})
-bs = BeautifulSoup(response.text, 'lxml')
-languages = bs.select('div.css-1ady226 a')
-urls = ''.join([url,languages[2].attrs.get('href').split('/')[1]])
+url = "https://www.rallit.com/hub"
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get(urls)
+driver.get(url)
 
 resume = []
-for i in tqdm(range(1, 37)):
-    try:
-        driver.find_element(By.XPATH, f'/html/body/div[1]/main/section/div[4]/div/div[2]/section/ul/li[{i}]/a/article').click()
-        time.sleep(4)
-        bs = BeautifulSoup(driver.page_source, 'lxml')
-        resume.append(bs.select('div.css-1799hu')[0].text)
-        time.sleep(5)
-        driver.get(urls)
-        time.sleep(9)
+while True:
+    for j in tqdm(range(1, 37)):
+        try:
+            driver.find_element(By.XPATH, f'/html/body/div[1]/main/section/div[4]/div/div[2]/section/ul/li[{j}]/a/article').click()
+            time.sleep(4)
+            bs = BeautifulSoup(driver.page_source, 'lxml')
+            resume.append(bs.select('div.css-1799hu')[0].text)
+            time.sleep(5)
+            driver.back()
+            time.sleep(9)
 
-    except NoSuchElementException:
-        driver.get(urls)
-        continue
-    except Exception as e:
-        driver.get(urls)
-        continue
-    except IndexError:
-        driver.get(urls)
-        continue
-    
+        except NoSuchElementException:
+            driver.back()
+            continue
+        except Exception:
+            driver.back()
+            continue
+        except IndexError:
+            driver.back()
+            continue
+
+    button = driver.find_element(By.XPATH, "/html/body/div[1]/main/section/div[4]/div/div[2]/section/div/div/button[7]")  # 버튼의 ID 사용
+    if button.get_attribute("disabled") != 'true':
+        button.click()
+        time.sleep(10)
+    else:
+        break
+
+with open("list.txt", "w") as file:
+    for item in resume:
+        file.write(item + "\n")
