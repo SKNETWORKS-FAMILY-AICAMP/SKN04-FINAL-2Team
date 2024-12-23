5eb8716506d5e8e298efd7cc25c90b16c5983deb
SWY <swypawn@gmail.com>
2024-12-23 12:31:20 +0900

    refactor: 사용자 정보 수정 URL 변경 🔄

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # JWT 갱신
     path('register/', RegisterAPIView.as_view(), name='register'),  # 회원가입
-    path('profile/', UserProfileAPIView.as_view(), name='profile'),  # 사용자 정보 조회
-    path('profile/update/', UserUpdateAPIView.as_view(), name='profile_update'),  # 사용자 정보 수정
+    path('accounts/', UserProfileAPIView.as_view(), name='profile'),  # 사용자 정보 조회
+    path('accounts/update/', UserUpdateAPIView.as_view(), name='profile_update'),  # 사용자 정보 수정
     
 ]
