a4e5addfc4ad2b1bb889bf6f50ca53bb3437b9a1
SWY <swypawn@gmail.com>
2025-01-08 08:51:53 +0900

    refactor: Admin 패널 검색 및 필터링 기능 개선 ✨

--- backend/profile_manager/admin.py
+++ backend/profile_manager/admin.py
@@ -13,19 +13,19 @@ class ProfileAdmin(admin.ModelAdmin):
 @admin.register(ProfileData)
 class ProfileDataAdmin(admin.ModelAdmin):
     list_display = ('profile', 'original_data', 'processed_data', 'pdf_data')
-    search_fields = ('profile__name',)
+    search_fields = ('profile__name', )
 
 @admin.register(TechStack)
 class TechStackAdmin(admin.ModelAdmin):
     list_display = ('profile', 'tech_stack_name')
-    search_fields = ('tech_stack_name',)
-    list_filter = ('tech_stack_name',)
+    search_fields = ('tech_stack_name', )
+    list_filter = ('tech_stack_name', )
 
 @admin.register(Career)
 class CareerAdmin(admin.ModelAdmin):
     list_display = ('profile', 'company_name', 'position', 'career_start_date', 'career_end_date', 'is_currently_employed')
     search_fields = ('company_name', 'position')
-    list_filter = ('is_currently_employed',)
+    list_filter = ('is_currently_employed', 'position', 'company_name' )
 
 @admin.register(AcademicRecord)
 class AcademicRecordAdmin(admin.ModelAdmin):
@@ -36,6 +36,7 @@ class AcademicRecordAdmin(admin.ModelAdmin):
 @admin.register(Certificate)
 class CertificateAdmin(admin.ModelAdmin):
     list_display = ('profile', 'certificate_name')
+    list_filter = ('certificate_name',)
     search_fields = ('certificate_name',)
 
 @admin.register(Language)
@@ -47,7 +48,7 @@ class LanguageAdmin(admin.ModelAdmin):
 @admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('user', 'profile', 'ai_analysis', 'created_at')
-    list_filter = ('created_at',)
+    list_filter = ('created_at', 'user')
     search_fields = ('user__username', 'profile__name')
     date_hierarchy = 'created_at'
     ordering = ('-created_at',)
