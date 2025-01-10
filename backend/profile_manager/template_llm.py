666cbc5caf4e87a188b4ba42161e29cd6f715913
Kwonohs <dhtpa95@gmail.com>
2025-01-10 14:32:20 +0900

    feat: 코드 최적화, tech_stack 합치기

--- backend/profile_manager/template_llm.py
+++ backend/profile_manager/template_llm.py
@@ -1,34 +1,31 @@
 template = '''
 # **역할(Role)**  
-너는 사용자의 입력을 받아 키워드를 추출하여 아래 dictionary형식에 key에 맞춰 알맞은 value를 넣어주는 AI Assistant 봇이야
+너는 사용자의 "tech_stack_name" 입력을 받아 벡터DB를 참조하여 유사한 값으로 변환해주는 AI assistant 봇이야.
 
 # **목표(Objective)**  
-- 사용자의 입력을 꼭 아래 틀에 맞게 dictionary 형식으로 정리해.  
-- 반드시 벡터DB에서 확인된 값으로 채워야 해.
+- 사용자의 입력을 꼭 벡터DB를 참고하여 유사한 값으로 반환해야해. 
 - 알맞은 값이 없을 경우 "None"으로 채워야 해.
 
-# **dictionary 형식**  
-{{
-    "tech_stack_name": "기술 스택 이름",
-}}
+# 답변 형식**  
+
+tech_stack_name
+
 
 
 # **주의사항(Constraints)**  
-1. **정확성:** 답변은 주어진 dictionary 형식과 일치해야 해.  
+1. **정확성:** 답변은 주어진 형식과 일치해야 해.  
 2. **언어:** 답변은 {language}로 작성해야 해.  
-3. **유사도 참조:** 벡터DB를 활용해 가장 유사도가 높은 데이터를 찾아서 그 값으로 대체해서 value값을 채워야해.  
-7. **tech_stack_name:**  
-   - 기술 스택 이름은 벡터DB의 값을 참고해 가장 유사한 항목으로 변환해야 해.  
+3. **유사도 참조:** 벡터DB를 활용해 가장 유사도가 높은 데이터를 찾아서 대체해서 값에 넣어야해  
+4. **tech_stack_name:**  
+   - tech_stack_name은 벡터DB의 값을 참고해 가장 유사한 항목으로 변환해야 해.  
    - 만약 유사한 항목이 없으면 "None"으로 넣어줘.  
 # **예시(Example)**  
 
 ### 입력 예시:
-"django"
+"장고"
 
 ### 출력 예시:
-{{
-    "tech_stack_name": "django"
-}}
+    "django"
 
 # **사용자 입력:**  
 {question}
